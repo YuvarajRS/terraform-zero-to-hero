d02d1972a2d42b1fc695c5774c966e82fcba2304
YuvarajRS <143590730+YuvarajRS@users.noreply.github.com>
2024-08-27 05:49:24 +0000

    Update the default value

--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -1,11 +1,14 @@
 variable "ami_value" {
     description = "value for the ami"
+    default = "ami-053b0d53c279acc90"
 }
 
 variable "instance_type_value" {
     description = "value for instance_type"
+    default = "t2.micro"
 }
 
 variable "subnet_id_value" {
     description = "value for the subnet_id"
+    default = "subnet-019ea91ed9b5252e7"
 }
